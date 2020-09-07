ad31cbcfbce6e44f9f26db085c3afcfa446c1576
oyebodefreeman <bodokunfolami@gmail.com>
2020-09-07 17:08:31 +0100

    Initial

---
+++ js/calulator.js
@@ -0,0 +1,42 @@
+const screen = document.querySelector('#id_screen');
+const buttons = document.querySelectorAll('input[type=button]');
+const themeBtn = document.querySelector('#theme-btn');
+const theme = document.querySelector('#theme');
+
+
+const onButtonClick = (e) => {
+    if (e.target.value === "=") {
+        evaluate();
+        return
+    } else if (e.target.value === "C") {
+        clear();
+        return
+    }
+    const number = e.target.value;
+    screen.value += number;
+}
+
+const clear = () => {
+    screen.value = "";
+}
+
+const evaluate = (e) => {
+    const result = eval(screen.value);
+    screen.value = result;
+}
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', onButtonClick);
+});
+
+
+themeBtn.addEventListener('click', (e) => {
+    if (theme.getAttribute('href').includes('dark.css')) {
+        theme.href = "css/light.css";
+        e.target.innerHTML = "Dark Mode 🌙";
+    } else {
+        theme.href = "css/dark.css";
+        e.target.innerHTML = "Light Mode ☀️";
+    }
+});
